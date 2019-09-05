eefe7f173f714830a49c9e48b1d1d12c58adfcec
bxf <593980428@qq.com>
2019-09-05 19:04:15 +0800

    列表

---
+++ resources/views/cetemeun/list.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<form action="{{url('category/doadd')}}" method="post" enctype="multipart/form-data">
+@csrf
+
+  <div class="form-group">
+  <h3>分类展示</h3>
+
+    <table class="table-striped table-bordered table-hover table-responsive" width="900" >
+        <tr align='center'>
+            <td>分类编号</td>
+            <td>分类名称</td>
+            <td>操作</td>
+        </tr>
+        
+            
+            @foreach($data as $v)
+            
+            <tr align='center'>
+                <td>{{$v->cate_id}}</td>
+                <td>{{$v->cate_name}}</td>
+                <td><a href="{{url('cetemeun/del',['id'=>$v->cate_id])}}">删除</a></td>
+
+
+            </tr>
+        @endforeach
+       
+
+
+    </table>
+
+@endsection
