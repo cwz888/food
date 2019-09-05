0771017cf83ac136423d675d4138a0689eadeb0b
bxf <593980428@qq.com>
2019-09-05 19:03:57 +0800

    列表

--- app/Http/Controllers/Admin/CatemenuController.php
+++ app/Http/Controllers/Admin/CatemenuController.php
@@ -8,18 +8,34 @@ use App\model\Catemenu;
 
 class CatemenuController extends Controller
 {
+    // 添加展示页面
     public function add()
     {
         // echo 1;
         return view('cetemeun.add');
     }
+
+    // 执行添加页面
     public function adddo(Request $request)
     {
         // echo 2;
+        // 接收所有值
         $data = $request->input();
         // dd($data);
+        // 把值入库
         $res = Catemenu::create($data);
-        return \json_encode(['code' => 200, 'msg' =>'添加成功'], JSON_UNESCAPED_UNICODE);
-        
+        if ($res) {
+            return \json_encode(['code' => 200, 'msg' => '添加成功'], JSON_UNESCAPED_UNICODE);
+        } else {
+            return \json_encode(['code' => 400, 'msg' => '添加失败'], JSON_UNESCAPED_UNICODE);
+        }
+    }
+
+    // 列表展示页面
+    public function list()
+    {
+        $data = Catemenu::get();
+        // dd($data);
+        return view('cetemeun.list',['data'=>$data]);
     }
 }
